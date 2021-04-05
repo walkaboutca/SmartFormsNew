3ec900ebb72f56c0e0a9149c28ef395d73c42d07
Craig Crawford <ccrawford@pbstools.ca>
2021-04-05 08:58:46 -0700

    Add project files.

---
+++ SmartForms/Web/FillableForms.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Web/forms.Master" CodeBehind="FillableForms.aspx.vb" Inherits="SmartForms.FillableForms" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <br /><br />
+    <div class="row">
+        <div class="col-md-4">
+            <telerik:RadButton ID="RadButton1" runat="server" Text="RadButton">
+            </telerik:RadButton>
+        </div>
+        <div class="col-md-4">
+           
+            <telerik:RadGrid ID="rgFields" runat="server" RenderMode="Lightweight"></telerik:RadGrid>
+
+        </div>
+        <div class="col-md-4">
+            <telerik:RadPdfViewer ID="pdfFormView" runat="server"></telerik:RadPdfViewer>
+        </div>
+    </div>
+
+
+
+</asp:Content>
---
+++ SmartForms/Web/FillableForms.aspx.vb
@@ -0,0 +1,41 @@
+﻿Imports System.IO
+Imports iText.Forms
+Imports iText.Forms.Fields
+Imports iText.Kernel.Pdf
+Imports Microsoft.AspNet.Identity
+Imports Telerik.Web.UI
+Imports Telerik.Windows.Documents.Flow.Model
+
+Public Class FillableForms
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub RadButton1_Click(sender As Object, e As EventArgs) Handles RadButton1.Click
+        Dim CurrentUserId As Guid = Guid.Parse(Me.Page.User.Identity.GetUserId())
+        Dim renderedBytes As Byte() = Nothing
+
+        Dim scr As String = "https://www.recbc.ca/pdf/forms/Disclosure-of-Representation-in-Trading-Services.pdf"
+
+        Dim reader As PdfReader = New PdfReader(scr)
+        Dim pdfDoc As PdfDocument = New PdfDocument(reader)
+
+        Dim dt As New DataTable
+        dt.Columns.Add("Key", GetType(String))
+        dt.Columns.Add("Value", GetType(String))
+
+        Dim form As PdfAcroForm = PdfAcroForm.GetAcroForm(pdfDoc, True)
+        Dim fields As IDictionary(Of String, PdfFormField) = form.GetFormFields()
+
+        For Each iKey As String In fields.Keys
+            dt.Rows.Add(iKey.ToString, Nothing)
+        Next
+        rgFields.DataSource = dt
+        rgFields.Rebind()
+
+    End Sub
+
+
+End Class
